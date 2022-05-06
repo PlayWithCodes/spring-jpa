3c6e88776c247d3b08a2bf6e2e88522d58685202
turtle <michael.app.space@gmail.com>
2022-05-07 07:41:47 +0900

    test: test paging and sorting

--- src/main/java/study/springjpa/dto/MemberDto.java
+++ src/main/java/study/springjpa/dto/MemberDto.java
@@ -1,6 +1,7 @@
 package study.springjpa.dto;
 
 import lombok.Data;
+import study.springjpa.entity.Member;
 
 @Data
 public class MemberDto {
@@ -13,5 +14,10 @@ public class MemberDto {
         this.id = id;
         this.username = username;
         this.teamname = teamname;
+    }
+
+    public MemberDto(Member member) {
+        this.id = member.getId();
+        this.username = member.getUsername();
     }
 }
